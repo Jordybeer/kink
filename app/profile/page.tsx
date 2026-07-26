00da760d4a8f0c2d1c84e02e31b517b7527d24da
jordybeer <info@jordy.beer>
2026-07-26 12:59:24 -0300

    Laat de vaste profieldeur hetzelfde scherm dragen

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -3,14 +3,14 @@
 import { Suspense, useMemo } from "react";
 import { useSearchParams } from "next/navigation";
 import PageShell from "@/components/PageShell";
-import ProfilePage from "./[id]/page";
+import ProfileScreen from "@/components/profile/ProfileScreen";
 
 function LocalProfileShell() {
   const searchParams = useSearchParams();
   const id = searchParams.get("id") ?? "";
   const params = useMemo(() => Promise.resolve({ id }), [id]);
 
-  return <ProfilePage params={params} />;
+  return <ProfileScreen params={params} />;
 }
 
 export default function ProfileQueryPage() {
