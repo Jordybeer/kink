a8c13936add15a1dd9e7284725384bd6ec2d787b
jordybeer <info@jordy.beer>
2026-07-31 23:07:41 -0300

    Use an exact WebCrypto signature buffer

--- lib/profileShareV3.ts
+++ lib/profileShareV3.ts
@@ -214,11 +214,13 @@ function bytesToBase64Url(bytes: Uint8Array): string {
   return btoa(binary).replace(/\+/g, "-").replace(/\//g, "_").replace(/=+$/g, "");
 }
 
-function base64UrlToBytes(value: string): Uint8Array {
+function base64UrlToBytes(value: string): Uint8Array<ArrayBuffer> {
   const padded = value.replace(/-/g, "+").replace(/_/g, "/")
     + "=".repeat((4 - (value.length % 4)) % 4);
   const binary = atob(padded);
-  return Uint8Array.from(binary, (char) => char.charCodeAt(0));
+  const bytes = new Uint8Array(binary.length);
+  for (let index = 0; index < binary.length; index++) bytes[index] = binary.charCodeAt(index);
+  return bytes;
 }
 
 function avatarProofMessage(proof: Omit<ProfileAvatarProof, "signature">): string {
